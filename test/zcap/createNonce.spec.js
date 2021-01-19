dfdc66cc98d9771e7d09f715a56d6f747c3fda58
Victor Grey <victorgrey@users.noreply.github.com>
2021-01-18 16:28:25 -0800

    Fixed typo.

--- test/zcap/createNonce.spec.js
+++ test/zcap/createNonce.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const auth = require('../../');
+const zcap = require('../../');
 
 describe('createNonce', function() {
   it('should create a unique nonce', function(){
     const nonces = [];
     for (let n = 0; n < 100; n++){
-      const nonce = auth.createNonce();
+      const nonce = zcap.createNonce();
       expect(nonce).to.be.aNonce();
       expect(nonces).to.not.include(nonce);
       nonces.push(nonce);
